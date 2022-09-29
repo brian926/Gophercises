bc92501bdeec793c41406622f3907f49b6ab72fd
Brian Antunes <brian.antunes@yahoo.com>
2022-09-29 11:19:49 -0400

    Added to CYOA

--- CYOA/main/main.go
+++ CYOA/main/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	cyoa ".."
 )
 
 func main() {
-	jsonFileName := flag.String("json", "../gopher.json", "JSON of the actual story")
+	port := flag.Int("port", 3000, "the port to start the web app on")
+	jsonFileName := flag.String("json", "gopher.json", "JSON of the actual story")
 	flag.Parse()
 
 	jsonFile, err := os.Open(*jsonFileName)
@@ -22,5 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- CYOA/story.go
+++ CYOA/story.go
@@ -2,20 +2,67 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(html))
+}
+
+var tpl *template.Template
+
+var html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adventure</title>
+</head>
+<body>
+    <h1>{{ .Title }}</h1>
+	{{range .Paragraphs}}
+		<p>{{.}}</p>
+	{{end}}
+	<ul>
+		{{range .Options}}
+			<li><a href="/{{.}}">{{.Text}}</li>
+		{{end}}
+	</ul>
+</body>
+</html>
+`
 
 type Story map[string]Chapter
 
 type Chapter struct {
-	Title   string   `json:"title"`
-	Story   []string `json:"story"`
-	Options []Option `json:"options"`
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
 }
 
 type Option struct {
 	Text string `json:"text"`
 	Arc  string `json:"arc"`
+}
+
+type handler struct {
+	s Story
+}
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func JsonStory(r io.Reader) (Story, error) {
