5552a75969738298328784ae7920cf38dba954d1
Brian Antunes <brian.antunes@yahoo.com>
2022-10-17 09:58:27 -0400

    Added to SiteMap

--- SiteMap/main/main.go
+++ SiteMap/main/main.go
@@ -4,6 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
+
+	link ".."
 )
 
 func main() {
@@ -13,7 +17,31 @@ func main() {
 	fmt.Println(*urlFlag)
 	resp, err := http.Get(*urlFlag)
 	if err != nil {
-
+		panic(err)
 	}
 	defer resp.Body.Close()
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+
+	base := baseUrl.String()
+
+	links, _ := link.Parse(resp.Body)
+	var hrefs []string
+
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
